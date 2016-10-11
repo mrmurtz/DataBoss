0146860a5011eef385a30855a47b77c099de4451
Alfie <alfie_ab@hotmail.co.uk>
2016-10-11 18:09:08 +0100

    tidying up of user diagram

--- public/src/diagram.js
+++ public/src/diagram.js
@@ -11,6 +11,11 @@ Diagram.prototype = {
     path.fillColor = "#EAF1F4";
     path.strokeColor = "#E06C72";
     path.strokeWidth = 4;
+    var from = new Point(50, 120);
+    var to = new Point(400, 120);
+    var line = new Path.Line(from, to);
+    line.strokeColor = "#E06C72";
+    line.strokeWidth = 4;
   },
 
   makeLine: function(){
@@ -22,36 +27,16 @@ Diagram.prototype = {
   },
 
   labelBox: function(value){
-    var text1 = new PointText(new Point(220, 100));
-    text1.justification = "center";
-    text1.fillColor = "black";
-    text1.content = value;
+    var text = new PointText(new Point(220, 100));
+    text.justification = "center";
+    text.fillColor = "black";
+    text.fontSize = 20;
+    text.content = value;
   },
-
-
-
-
-  // var rectangle2 = new Rectangle(new Point(700,50), new Point(1050,500));
-  // var cornersize = new Size(10, 10);
-  // var path = new Path.RoundRectangle(rectangle2, cornersize);
-  // path.fillColor = "#EAF1F4";
-  // path.strokeColor = "#E06C72";
-  // path.strokeWidth = 4;
-  //
-  // var text2 = new PointText(new Point(850, 100));
-  // text2.justification = "center";
-  // text2.fillColor = "black";
-  // text2.content = "Tweets";
-
-  // var text = new PointText(new Point(550, 200));
-  // text.justification = "center";
-  // text.fillColor = "black";
-  // text.content = "One to many";
-
 };
 
-
-
+var UserSelection = {language: "1", framework: "1", orm: "1", db: "1", tableName: "user"};
+var label = UserSelection.tableName;
 
 
 $(document).ready(function(){
@@ -60,15 +45,11 @@ $(document).ready(function(){
 
   $('#create-diagram').click(function(){
     diagram.makeBox();
+    diagram.labelBox(label);
   });
 
   $('#create-line').click(function(){
     diagram.makeLine();
-  });
-
-  $('input[name=label-btn]').click(function(){
-    var label = $('input[name=label-name]').val();
-    diagram.labelBox(label);
   });
 
 });
