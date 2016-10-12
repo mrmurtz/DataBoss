5a9557898106a416bef3c7ec746796feea61cc4a
Matthew Perkins <mfperkins@gmail.com>
2016-10-12 14:08:33 +0100

    refactoring

--- public/src/diagram.js
+++ public/src/diagram.js
@@ -7,7 +7,6 @@ var UserSelection = {language: "1", framework: "1", orm: "1", db: "1", tableName
 var label = UserSelection.tableName1;
 var property = UserSelection.columnName;
 
-
 // CREATING A RECTANGLE
 var rectangle1 = new Rectangle(new Point(50,50), new Point(400,500));
 var cornersize1 = new Size(10, 10);
@@ -47,73 +46,3 @@ var model1 = new Group ({
 var model2 = model1.clone();
 model2.strokeColor = 'black';
 model2.position = new Point(800, 275);
-
-
-
-
-Diagram.prototype = {
-
-  makeBox: function() {
-    var rectangle1 = new Rectangle(new Point(50,50), new Point(400,500));
-    var cornersize = new Size(10, 10);
-    var path = new Path.RoundRectangle(rectangle1, cornersize);
-    path.fillColor = "#EAF1F4";
-    path.strokeColor = "#E06C72";
-    path.strokeWidth = 4;
-    var header = new Path();
-    var hp1 = new Point(50, 120);
-    var hp2 = new Point(400, 120);
-    header.add(hp1);
-    header.add(hp2);
-    line.strokeColor = "#E06C72";
-    line.strokeWidth = 4;
-  },
-
-  makeLine: function(){
-    var path = new Path();
-    var point1 = new Point(400, 250);
-    var point2 = new Point(700, 250);
-    path.strokeColor = 'green';
-    path.add(point1);
-    path.add(point2);
-    line.strokeColor = "#E06C72";
-    line.strokeWidth = 4;
-  },
-
-  labelBox: function(value){
-    var text = new PointText(new Point(220, 100));
-    text.justification = "center";
-    text.fillColor = "black";
-    text.fontSize = 25;
-    text.content = value;
-  },
-
-  makeColumn: function(value) {
-    var text = new PointText(new Point(80, 160))
-    text.justification = "left";
-    text.fillColor = "black";
-    text.fontSize = 15;
-    text.content = value + ":";
-  }
-};
-
-
-$(document).ready(function(){
-
-  var diagram = new Diagram();
-
-
-
-
-  $('#create-diagram').click(function(){
-    // diagram.makeBox();
-    // diagram.labelBox(label);
-    // diagram.makeColumn(column);
-    // model
-  });
-
-  $('#create-line').click(function(){
-    diagram.makeLine();
-  });
-
-});
