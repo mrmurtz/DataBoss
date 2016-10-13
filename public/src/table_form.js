a89a96e5a9c635346d39f8e1c54a1c29b2695106
Stéphan Gaud <stephgoeseast@gmail.com>
2016-10-13 18:07:04 +0100

    Implemented clone functionality for column block

--- public/src/table_form.js
+++ public/src/table_form.js
@@ -1,25 +1,51 @@
 var regex = /^(.+?)(\d+)$/i;
-var cloneIndex = $(".clonedInput").length;
+var tableIndex = $(".clonedInput").length;
 
 function clone(){
     $('#one-entry').clone()
         .appendTo("#table")
-        .attr("id", cloneIndex)
+        .attr("id", tableIndex)
         .find("*")
         .each(function() {
             var id = this.id || "";
             var match = id.match(regex) || [];
             if (match.length == 3) {
-                this.id = match[1] + (cloneIndex);
+                this.id = match[1] + (tableIndex);
             }
         })
         .on('click', 'button.clone', clone)
         .on('click', 'button.remove', remove);
-    cloneIndex++;
+    tableIndex++;
 }
 function remove(){
-    $(this).parents("#table").remove();
+    $("#0").remove();
 }
 $("button.clone").on("click", clone);
 
 $("button.remove").on("click", remove);
+
+
+var columnIndex = $(".clonedInput").length;
+
+function addColumn(){
+    $('#column-entry').clone()
+        .appendTo("#column")
+        .attr("id", columnIndex)
+        .find("*")
+        .each(function() {
+            var id = this.id || "";
+            var match = id.match(regex) || [];
+            if (match.length == 3) {
+                this.id = match[1] + (columnIndex);
+            }
+        })
+        .on('click', 'button.addColumn', addColumn)
+        .on('click', 'button.removeColumn', removeColumn);
+    columnIndex++;
+}
+function removeColumn(){
+    $("#0").remove();
+}
+$("button.addColumn").on("click", addColumn);
+
+$("button.removeColumn").on("click", removeColumn);
